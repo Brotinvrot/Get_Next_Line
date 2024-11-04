ad22f37def4a7ba9c3252c0e19671f5c96bece57
drabadan <rodina05region@gmail.com>
2024-11-04 12:02:25 +0100

    At this stage, everything is stable and there are no leaks. However, I need to bring everything to NORM and there is a problem with the output of the last line. It is buffered and collected in the variable line, but we need to implement a solution for returning and releasing a static variable.

--- main.c
+++ main.c
@@ -6,11 +6,12 @@
 /*   By: drabadan <drabadan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 17:38:52 by drabadan          #+#    #+#             */
-/*   Updated: 2024/11/03 11:14:48 by drabadan         ###   ########.fr       */
+/*   Updated: 2024/11/04 11:19:38 by drabadan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "gnl_head.h"
+#include <stdio.h>
 
 int	find_n(char *line)
 {
@@ -82,26 +83,61 @@ char	*get_next_line(int fd)
 	char		*bufer;
 	int			chek_read;
 	char		*tmp;
-	static char	*line;
+	char		*line;
+	static char	last_read[BUFER_SIZE] = "";
+	int			i;
 
+// Требуется правильный цикл который будет обрабатывать накопленный бафер и лайн
+	line = ft_strdup(last_read);
 	while (find_n(line) != 0)
 	{
 		bufer = malloc(BUFER_SIZE + 1);
-		chek_read = read(fd, bufer, BUFER_SIZE);
-		if (!bufer || chek_read <= 0)
-			return (no_bit_return(&line, bufer));
-		if (!line)
-			line = ft_strdup("");
+		if (!bufer)
+		{
+			free(bufer);
+			return (NULL);
+		}
 		bufer[chek_read] = '\0';
+		chek_read = read(fd, bufer, BUFER_SIZE);
+		if (chek_read <= 0)
+		{
+			free(bufer);
+			return (NULL);
+		}
+		if(find_n(line) == 0)
+		{
+			free(bufer);
+			free(tmp);
+			break ;
+		}
 		tmp = line;
 		line = ft_strjoin(line, bufer);
-		free(bufer);
+		printf("LINE = %s\n", line);
 		free(tmp);
+		free(bufer);
 	}
+
+// Вызов двух функций : Первая - для извлечения из общей массы сроки до "\n"
+//						Вторая - для удаления этой строки из общей массы
+
 	tmp = str_until_char(line, '\n');
 	get_clean(&line, tmp);
+
+// Заполнение статичной переменной остатком
+	i = 0;
+	while(line[i])
+	{
+		last_read[i] = line[i];
+		i++;
+	}
+	last_read[i] = '\0';
+	// printf("OSTATOK = %s\n", last_read);
+	// printf("LINE    = %s\n", line);
+	free(line);
 	return (tmp);
 }
+
+//  И тут || )// Проблема тут return (no_bit_return(&line, bufer));
 
 int	main(int argc, char **argv)
 {
@@ -119,14 +155,13 @@ int	main(int argc, char **argv)
 		write(2, "Error: could not open file\n", 27);
 		return (1);
 	}
-	// while ((line = get_next_line(fd)) != NULL)
-	// {
-	// 	write(1, line, ft_strlen(line));
-	// 	write(1, "\n", 1);
-	// 	free(line);
-	// }
-	line = get_next_line(fd);
-	write(1, line, ft_strlen(line));
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		write(1, line, ft_strlen(line));
+		free(line);
+	}
+	// line = get_next_line(fd);
+	// write(1, line, ft_strlen(line));
 	free(line);
 	close(fd);
 	return (0);
